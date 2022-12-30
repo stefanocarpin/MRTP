f9bba129c970c320ff7afaa35e3852ac8ac0bc60
Stefano Carpin <stefanocarpin@protonmail.com>
2022-12-30 08:02:26 -0800

    Fixed names

--- MRTP/src/talklisten/src/listener.cpp
+++ MRTP/src/talklisten/src/listener.cpp
@@ -1,22 +1,20 @@
 #include <rclcpp/rclcpp.hpp>
 #include <std_msgs/msg/string.hpp>
-#include <iostream>
 
-rclcpp::Node::SharedPtr nh_;
+rclcpp::Node::SharedPtr nodeh;
 
 void callback(const std_msgs::msg::String::SharedPtr msg) {
-  RCLCPP_INFO(nh_->get_logger(),"Received: %s",msg->data.c_str());
+  RCLCPP_INFO(nodeh->get_logger(),"Received: %s",msg->data.c_str());
 }
 
 int main(int argc,char **argv) {
 
   rclcpp::init(argc,argv);
-  std::cout<< "Starting..." << std::endl ;
-  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr string_sub_;
-  
-  nh_ = rclcpp::Node::make_shared("listener");
-  string_sub_ = nh_->create_subscription<std_msgs::msg::String>("message",10,&callback);
-  rclcpp::spin(nh_);
+  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr sub;
+  nodeh = rclcpp::Node::make_shared("listener");
+  sub = nodeh->create_subscription<std_msgs::msg::String>
+                                             ("message",10,&callback);
+  rclcpp::spin(nodeh);
  
   rclcpp::shutdown();
   return 0;
--- MRTP/src/talklisten/src/talker.cpp
+++ MRTP/src/talklisten/src/talker.cpp
@@ -5,22 +5,21 @@
 int main(int argc,char **argv) {
 
   rclcpp::init(argc,argv);
-  std::cout<< "Starting..." << std::endl ;
 
-  rclcpp::Node::SharedPtr nh_;
-  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr string_pub_;
+  rclcpp::Node::SharedPtr nodeh;
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr pub;
   rclcpp::Rate rate(1);
   
-  nh_ = rclcpp::Node::make_shared("talker");
-  string_pub_ = nh_->create_publisher<std_msgs::msg::String>("message",1);
+  nodeh = rclcpp::Node::make_shared("talker");
+  pub = nodeh->create_publisher<std_msgs::msg::String>("message",1);
 
   int counter = 0;
   while ( (counter++ < 100) && (rclcpp::ok())  ) {
-    RCLCPP_INFO(nh_->get_logger(),"Sending message #%d",counter);
+    RCLCPP_INFO(nodeh->get_logger(),"Sending message #%d",counter);
     std_msgs::msg::String stringtosend;
     stringtosend.data = "Message # " + std::to_string(counter);
-    string_pub_->publish(stringtosend);
-    rclcpp::spin_some(nh_);
+    pub->publish(stringtosend);
+    rclcpp::spin_some(nodeh);
     rate.sleep();
   } 
   rclcpp::shutdown();
