a83fc0bbd30d8f94040a3c231ef036e88d5771cb
Stefano Carpin <stefanocarpin@protonmail.com>
2022-12-30 06:33:00 -0800

    Compressed format

--- MRTP/src/talklisten/src/listener.cpp
+++ MRTP/src/talklisten/src/listener.cpp
@@ -8,13 +8,10 @@ void callback(const std_msgs::msg::String::SharedPtr msg) {
   RCLCPP_INFO(nh_->get_logger(),"Received: %s",msg->data.c_str());
 }
 
-
 int main(int argc,char **argv) {
 
   rclcpp::init(argc,argv);
   std::cout<< "Starting..." << std::endl ;
-
- 
   rclcpp::Subscription<std_msgs::msg::String>::SharedPtr string_sub_;
   
   nh_ = rclcpp::Node::make_shared("listener");
@@ -22,7 +19,6 @@ int main(int argc,char **argv) {
   rclcpp::spin(nh_);
  
   rclcpp::shutdown();
-  std::cout << "Done!"<< std::endl;
   return 0;
   
 }
