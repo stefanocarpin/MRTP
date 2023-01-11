4d4ebe639d2c4c3d4c55a789ff687d703b2cc537
Stefano Carpin <stefanocarpin@protonmail.com>
2023-01-11 10:42:42 -0800

    added comments and formatting

--- MRTP/src/examples/src/drawsquarefb.cpp
+++ MRTP/src/examples/src/drawsquarefb.cpp
@@ -6,10 +6,10 @@ float x, y, theta;
 bool init;
 
 void poseCallback(const turtlesim::msg::Pose::SharedPtr msg) {
-    x = msg->x;
-    y = msg->y;
-    theta = msg->theta;
-    init = true;
+  x = msg->x;
+  y = msg->y;
+  theta = msg->theta;
+  init = true;
 }
 
 int main(int argc,char **argv) {
@@ -20,7 +20,7 @@ int main(int argc,char **argv) {
 
   nodeh = rclcpp::Node::make_shared("drawsquarefb");
   auto pub = nodeh->create_publisher<geometry_msgs::msg::Twist>
-      ("turtle1/cmd_vel",1000);
+    ("turtle1/cmd_vel",1000);
   auto sub = nodeh->create_subscription<turtlesim::msg::Pose>
     ("turtle1/pose",1000,&poseCallback);
 
@@ -30,37 +30,38 @@ int main(int argc,char **argv) {
   bool rotate = false;
   float start_x = x;
   float start_y = y;
-  double DIRS[] = {0,M_PI/2,-M_PI,-M_PI/2};
-  int direction = 0;
+  double DIRS[] = {0,M_PI/2,-M_PI,-M_PI/2}; // desired directions
+  int direction = 0; // current direction
 
-  while (!init)
-      rclcpp::spin_some(nodeh);
+  while (!init) // wait for at least one sensor reading
+    rclcpp::spin_some(nodeh);
   
   while (rclcpp::ok()) {
-      rclcpp::spin_some(nodeh);
-      if (rotate) {
-	if ( ( ( direction == 0  ) && ( theta < DIRS[0] ) ) ||
-	     ( ( direction == 1  ) && ( theta < DIRS[1] ) ) ||
-	     ( ( direction == 2  ) && ( theta  >0 ) ) ||
-	     ( ( direction == 3  ) && ( theta < DIRS[3] ) ) ) {
-	    msg.linear.x = 0; msg.angular.z = M_PI/8;
-	  }
-	else {
-	  msg.linear.x = 0; msg.angular.z = 0;
-	  rotate = false;
-	  start_x = x; start_y = y;
-	}
+    rclcpp::spin_some(nodeh); //get sensor reading, if available
+    if (rotate) { // rotating?
+      // reached desired heading?
+      if ( ( ( direction == 0  ) && ( theta < DIRS[0] ) ) ||
+	   ( ( direction == 1  ) && ( theta < DIRS[1] ) ) ||
+	   ( ( direction == 2  ) && ( theta  > 0 ) ) ||
+	   ( ( direction == 3  ) && ( theta < DIRS[3] ) ) ) {
+	msg.linear.x = 0; msg.angular.z = M_PI/8; // no; keep rotating
       }
-      else {
-	  if (hypotf((x-start_x),(y-start_y))<2) {
-	      msg.linear.x = 0.5; msg.angular.z = 0; 
-	  }
-	  else {
-	      rotate = true;
-	      msg.linear.x = 0; msg.angular.z = 0;
-	      ++direction %= 4;
-	  }
+      else { // yes
+	msg.linear.x = 0; msg.angular.z = 0; //stop the robot
+	rotate = false; // switch to translating
+	start_x = x; start_y = y; // record current location
       }
-      pub->publish(msg);
+    }
+    else { // translating?
+      if (hypotf((x-start_x),(y-start_y))<2) { // moved less than 2 units?
+	msg.linear.x = 0.5; msg.angular.z = 0;  // no, keep moving forward
+      }
+      else { // moved 2 units
+	rotate = true; // switch to rotate
+	msg.linear.x = 0; msg.angular.z = 0; // stop the robot
+	++direction %= 4; // track next direction
+      }
+    }
+    pub->publish(msg); // send motion command
   }
 }
