913a2c80218cfe89c58ac1e55e6799404d86e29d
stefanocarpin <stefanocarpin@protonmail.com>
2024-11-11 13:47:07 -0800

    updated talklistener packate to jazzy

--- MRTP/src/talklisten/src/talker.cpp
+++ MRTP/src/talklisten/src/talker.cpp
@@ -1,5 +1,5 @@
 /*
-Copyright 2023 Stefano Carpin
+Copyright 2024 Stefano Carpin
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
