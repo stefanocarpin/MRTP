9a61d13732a0b57fa9ae25d98126854c5c7d0be4
stefanocarpin <stefanocarpin@protonmail.com>
2024-12-01 16:20:11 -0800

    upated version

--- MRTP/src/gazeboenvs/setup.py
+++ MRTP/src/gazeboenvs/setup.py
@@ -6,13 +6,13 @@ package_name = 'gazeboenvs'
 
 setup(
     name=package_name,
-    version='0.0.1',
+    version='0.1',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name,'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name,'launch'), glob('launch/*_launch.py')),
         (os.path.join('share', package_name,'worlds/'), glob('./worlds/*')),
         (os.path.join('share', package_name,'models/pioneer2dx/'), glob('./models/pioneer2dx/*')),
         (os.path.join('share', package_name,'models/pioneer3at/'), glob('./models/pioneer3at/*')),
