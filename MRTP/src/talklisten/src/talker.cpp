6d39fda537eb090e148f399dac1586d1452d64c4
stefanocarpin <stefanocarpin@protonmail.com>
2025-02-02 21:11:37 -0800

    upgraded talklistener to get rid of std_msg

--- MRTP/src/talklisten/src/talker.cpp
+++ MRTP/src/talklisten/src/talker.cpp
@@ -15,24 +15,24 @@ limitations under the License.
 */
 
 #include <rclcpp/rclcpp.hpp> // needed for basic functions
-#include <std_msgs/msg/string.hpp> // needed because we publish strings
+#include <example_interfaces/msg/string.hpp> // needed because we publish strings
 
 int main(int argc,char **argv) {
 
   rclcpp::init(argc,argv); // initialize the ROS subsystem
 
   rclcpp::Node::SharedPtr nodeh;
-  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr pub;
+  rclcpp::Publisher<example_interfaces::msg::String>::SharedPtr pub;
   rclcpp::Rate rate(1); // create rate object
   
   nodeh = rclcpp::Node::make_shared("talker"); // create node instance
   // create publisher to topic "message" of strings
-  pub = nodeh->create_publisher<std_msgs::msg::String>("message",1);
+  pub = nodeh->create_publisher<example_interfaces::msg::String>("message",1);
 
   int counter = 0;
   while ( (counter++ < 100) && (rclcpp::ok())  ) {
     RCLCPP_INFO(nodeh->get_logger(),"Sending message #%d",counter);
-    std_msgs::msg::String stringtosend;
+    example_interfaces::msg::String stringtosend;
     // prepare message to send
     stringtosend.data = "Message # " + std::to_string(counter);
     pub->publish(stringtosend); // publish message
