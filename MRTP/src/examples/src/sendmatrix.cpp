2995d4969db21dfddf301cceaeeb79979e1229b1
stefanocarpin <stefanocarpin@protonmail.com>
2025-02-08 14:41:57 -0800

    fixed matrix example

--- MRTP/src/examples/src/sendmatrix.cpp
+++ MRTP/src/examples/src/sendmatrix.cpp
@@ -15,46 +15,46 @@ limitations under the License.
 */
 
 #include <rclcpp/rclcpp.hpp>
-#include <std_msgs/msg/int32_multi_array.hpp>
-#include <std_msgs/msg/multi_array_dimension.hpp>
+#include <example_interfaces/msg/int32_multi_array.hpp>
+#include <example_interfaces/msg/multi_array_dimension.hpp>
 
 #define ROWS 4 // matrix dimensions
 #define COLS 5
 
 int main(int argc,char **argv) {  
-    rclcpp::init(argc,argv);
-    rclcpp::Node::SharedPtr nodeh;
-    rclcpp::Rate rate(1);
+  rclcpp::init(argc,argv);
+  rclcpp::Node::SharedPtr nodeh;
+  rclcpp::Rate rate(1);
 
-    nodeh = rclcpp::Node::make_shared("sendmatrix"); // create node
-    // create publisher
-    auto pubA = nodeh->create_publisher<std_msgs::msg::Int32MultiArray>
+  nodeh = rclcpp::Node::make_shared("sendmatrix"); // create node
+  // create publisher
+  auto pubA = nodeh->create_publisher<example_interfaces::msg::Int32MultiArray>
       ("matrixint",10);
 
-    // instance of message to send
-    std_msgs::msg::Int32MultiArray toSend;
-    int value;
+  // instance of message to send
+  example_interfaces::msg::Int32MultiArray toSend;
+  int value;
     
-    // setup layout for a matrix of size ROWS * COLS
-    toSend.layout.dim.resize(2);  // dimensions
-    toSend.layout.dim[0].size = ROWS; // size of the first dimension
-    toSend.layout.dim[0].stride = ROWS * COLS; // not necessarily needed 
-    toSend.layout.dim[0].label = "row"; // label for first dimension
-    toSend.layout.dim[1].size = COLS; // size of second dimension
-    toSend.layout.dim[1].stride = COLS; // separation between columns
-    toSend.layout.dim[1].label = "col"; // label for second dimension
-    toSend.layout.data_offset = 0; // no offset
-    toSend.data.resize(toSend.layout.dim[0].stride); // bumber of elements
-    while (rclcpp::ok()) {
-      // fills entry (i,j) with i*j+value
-      for (int i = 0; i < ROWS ; i++) {
-	for ( int j = 0 ; j < COLS ; j++ ) {
-	  // note how access (i,j) in data
-	  toSend.data[i*toSend.layout.dim[1].stride + j] = i*j+value;
-	}
+  // setup layout for a matrix of size ROWS * COLS
+  toSend.layout.dim.resize(2);  // dimensions
+  toSend.layout.dim[0].size = ROWS; // size of the first dimension
+  toSend.layout.dim[0].stride = ROWS * COLS; // not necessarily needed 
+  toSend.layout.dim[0].label = "row"; // label for first dimension
+  toSend.layout.dim[1].size = COLS; // size of second dimension
+  toSend.layout.dim[1].stride = COLS; // separation between columns
+  toSend.layout.dim[1].label = "col"; // label for second dimension
+  toSend.layout.data_offset = 0; // no offset
+  toSend.data.resize(toSend.layout.dim[0].stride); // bumber of elements
+  while (rclcpp::ok()) {
+    // fills entry (i,j) with i*j+value
+    for (int i = 0; i < ROWS ; i++) {
+      for ( int j = 0 ; j < COLS ; j++ ) {
+	// note how access (i,j) in data
+	toSend.data[i*toSend.layout.dim[1].stride + j] = i*j+value;
       }
-      value++;
-      pubA->publish(toSend); // publish
-      rate.sleep();
     }
+    value++;
+    pubA->publish(toSend); // publish
+    rate.sleep();
+  }
 }
