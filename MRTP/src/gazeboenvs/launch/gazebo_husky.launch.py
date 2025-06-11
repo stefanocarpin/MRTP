411931493c26329c2a054b9b39d84aa3f4f565a5
stefanocarpin <stefanocarpin@protonmail.com>
2025-06-11 12:15:47 -0700

    fixed gazebo launch fies for chapter 4

--- MRTP/src/gazeboenvs/launch/gazebo_husky.launch.py
+++ MRTP/src/gazeboenvs/launch/gazebo_husky.launch.py
@@ -1,4 +1,4 @@
-# Copyright 2019 Open Source Robotics Foundation, Inc.
+# Copyright 2023 Clearpath Robotics, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,37 +11,80 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+# @author Roni Kreinin (rkreinin@clearpathrobotics.com)
 
-"""
-Demo for spawn_entity.
-Launches Gazebo and spawns a model
-"""
-
-"""
-Tweaked to load MRTP files
-"""
-
-
-import os  
-from launch import LaunchDescription 
-from launch_ros.actions import Node 
-from launch.actions import ExecuteProcess 
 from ament_index_python.packages import get_package_share_directory
 
-def generate_launch_description():
-    
-    pkg_dir = get_package_share_directory('gazeboenvs')
-    world = os.path.join(pkg_dir, 'worlds', 'empty.world')
-    sdf_path = os.path.join(pkg_dir, 'models', 'husky', 'model.sdf')
-    os.environ["GAZEBO_MODEL_PATH"] = os.path.join(pkg_dir, 'models')
-    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
-                        arguments=['-entity', 'demo', '-file', sdf_path],
-                        output='screen')
-  
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import EnvironmentVariable, LaunchConfiguration, PathJoinSubstitution
 
-    return LaunchDescription([
-         ExecuteProcess(
-            cmd=['gazebo', '--verbose', world, '-s', 'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so'],
-            output='screen'),
-        spawn_entity,
-    ])
+
+ARGUMENTS = [
+    DeclareLaunchArgument('rviz', default_value='false',
+                          choices=['true', 'false'], description='Start rviz.'),
+    DeclareLaunchArgument('world', default_value='warehouse',
+                          choices=[
+                              'construction',
+                              'office',
+                              'orchard',
+                              'pipeline',
+                              'solar_farm',
+                              'warehouse',
+                          ],
+                          description='Gazebo World'),
+    DeclareLaunchArgument('setup_path',
+                          default_value=[EnvironmentVariable('HOME'), '/clearpath/'],
+                          description='Clearpath setup path'),
+    DeclareLaunchArgument('use_sim_time', default_value='true',
+                          choices=['true', 'false'],
+                          description='use_sim_time'),
+]
+
+for pose_element in ['x', 'y', 'yaw']:
+    ARGUMENTS.append(DeclareLaunchArgument(pose_element, default_value='0.0',
+                     description=f'{pose_element} component of the robot pose.'))
+
+ARGUMENTS.append(DeclareLaunchArgument('z', default_value='0.3',
+                 description='z component of the robot pose.'))
+
+
+def generate_launch_description():
+    # Directories
+    pkg_clearpath_gz = get_package_share_directory(
+        'clearpath_gz')
+
+    # Paths
+    gz_sim_launch = PathJoinSubstitution(
+        [pkg_clearpath_gz, 'launch', 'gz_sim.launch.py'])
+    robot_spawn_launch = PathJoinSubstitution(
+        [pkg_clearpath_gz, 'launch', 'robot_spawn.launch.py'])
+
+    gz_sim = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([gz_sim_launch]),
+        launch_arguments=[
+            ('world', LaunchConfiguration('world'))
+        ]
+    )
+
+    robot_spawn = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([robot_spawn_launch]),
+        launch_arguments=[
+            ('use_sim_time', LaunchConfiguration('use_sim_time')),
+            ('setup_path', LaunchConfiguration('setup_path')),
+            ('world', LaunchConfiguration('world')),
+            ('rviz', LaunchConfiguration('rviz')),
+            ('x', LaunchConfiguration('x')),
+            ('y', LaunchConfiguration('y')),
+            ('z', LaunchConfiguration('z')),
+            ('yaw', LaunchConfiguration('yaw'))]
+    )
+
+    # Create launch description and add actions
+    ld = LaunchDescription(ARGUMENTS)
+    ld.add_action(gz_sim)
+    ld.add_action(robot_spawn)
+    return ld
--- MRTP/src/gazeboenvs/launch/gazebo_p3at_launch.py
+++ MRTP/src/gazeboenvs/launch/gazebo_p3at_launch.py
@@ -22,26 +22,98 @@ Tweaked to load MRTP files
 """
 
 
-import os  
+import os
+import tempfile
+
 from launch import LaunchDescription 
 from launch_ros.actions import Node 
 from launch.actions import ExecuteProcess 
-from ament_index_python.packages import get_package_share_directory 
+from ament_index_python.packages import get_package_share_directory
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import (
+    AppendEnvironmentVariable,
+    DeclareLaunchArgument,
+    ExecuteProcess,
+    IncludeLaunchDescription,
+    OpaqueFunction,
+    RegisterEventHandler,
+)
+
+from launch.conditions import IfCondition
+from launch.substitutions import Command, LaunchConfiguration, PythonExpression
+
 
 def generate_launch_description():
     
     pkg_dir = get_package_share_directory('gazeboenvs')
+    sim_dir = get_package_share_directory('nav2_minimal_tb4_sim')
     world = os.path.join(pkg_dir, 'worlds', 'test.world')
     sdf_path = os.path.join(pkg_dir, 'models', 'pioneer3at', 'model.sdf')
     os.environ["GAZEBO_MODEL_PATH"] = os.path.join(pkg_dir, 'models')
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
                         arguments=['-entity', 'demo', '-file', sdf_path],
                         output='screen')
-  
 
-    return LaunchDescription([
-         ExecuteProcess(
-            cmd=['gazebo', '--verbose', world, '-s', 'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so'],
-            output='screen'),
-        spawn_entity,
-    ])
+    use_simulator = LaunchConfiguration('use_simulator')
+    headless = LaunchConfiguration('headless')
+    world = LaunchConfiguration('world')
+    
+
+    declare_use_simulator_cmd = DeclareLaunchArgument(
+        'use_simulator',
+        default_value='True',
+        description='Whether to start the simulator',
+    )
+    
+    declare_simulator_cmd = DeclareLaunchArgument(
+        'headless', default_value='False', description='Whether to execute gzclient)'
+    )
+
+    declare_world_cmd = DeclareLaunchArgument(
+        'world',
+        default_value=os.path.join(sim_dir, 'worlds', 'depot.sdf'),  # Try warehouse.sdf!
+        description='Full path to world model file to load',
+    )
+
+    world_sdf = tempfile.mktemp(prefix='nav2_', suffix='.sdf')
+    world_sdf_xacro = ExecuteProcess(
+        cmd=['xacro', '-o', world_sdf, ['headless:=', headless], world])
+    
+    gazebo_server = ExecuteProcess(
+        cmd=['gz', 'sim', '-r', '-s', world_sdf],
+        output='screen',
+        condition=IfCondition(use_simulator)
+    )
+
+    set_env_vars_resources = AppendEnvironmentVariable(
+            'GZ_SIM_RESOURCE_PATH',
+            os.path.join(sim_dir, 'worlds'))
+    gazebo_client = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory('ros_gz_sim'),
+                         'launch',
+                         'gz_sim.launch.py')
+        ),
+        condition=IfCondition(PythonExpression(
+            [use_simulator, ' and not ', headless])),
+        launch_arguments={'gz_args': ['-v4 -g ']}.items(),
+    )
+    
+    ld = LaunchDescription()
+    ld.add_action(declare_use_simulator_cmd)
+    ld.add_action(declare_simulator_cmd)
+    ld.add_action(declare_world_cmd)
+    
+    ld.add_action(gazebo_server)
+    ld.add_action(gazebo_client)
+    
+    return ld
+
+    #return LaunchDescription([
+    #     ExecuteProcess(
+    #        cmd=['gazebo', '--verbose', world, '-s', 'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so'],
+    #        output='screen'),
+    #    spawn_entity,
+    #])
+
+
