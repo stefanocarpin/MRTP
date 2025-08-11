01f562eaf870122b45a98216193ae8d200062ad9
stefanocarpin <stefanocarpin@protonmail.com>
2025-08-10 22:20:46 -0700

    added python example with parameters from a YAML file

---
+++ MRTP/src/examples/launch/paramfile.launch.py
@@ -0,0 +1,34 @@
+"""
+Copyright 2025 Stefano Carpin
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.substitutions import PathJoinSubstitution
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+
+    pkg_examples = get_package_share_directory('examples')
+    params_file_path = PathJoinSubstitution(
+        [pkg_examples, 'launch', 'parameters.yaml'])
+    
+    return LaunchDescription([
+        Node(
+            package='examples',
+            executable='paramclient',
+            parameters=[params_file_path]
+        )
+    ])
